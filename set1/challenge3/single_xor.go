738d8ff9445078df33f386907fef6e1545d527e8
Sammy Hasan <sammy.nm.hasan@gmail.com>
2025-03-16 11:29:57 +0000

    Improve solution automation

--- set1/challenge3/single_xor.go
+++ set1/challenge3/single_xor.go
@@ -3,15 +3,20 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
 )
 
 func main() {
+	var freq string = "etaoin shrdlu"
+
 	decoded_bytes, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
 	fmt.Println(decoded_bytes)
 
 	n := len(decoded_bytes)
 
+	total_score := 0
+	var candidate string
 	for k := range 256 {
 		xored := []byte{}
 		for i := range n {
@@ -20,6 +25,18 @@ func main() {
 		}
 
 		encoded_str := string(xored)
-		fmt.Println("K: ", k, "> ", encoded_str)
+		score := 0
+		for _, char := range freq {
+			c := strings.Count(encoded_str, string(char))
+			score += c
+		}
+
+		if score > total_score {
+			total_score = score
+			candidate = encoded_str
+		}
+
 	}
+
+	fmt.Println(candidate)
 }
